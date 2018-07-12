a8010e79ccda7bf6f0e0f92feac928120c8b764b
micha <michael.king@uni-konstanz.de>
2018-07-12 17:00:30 +0200

    add mapping function

---
+++ development/lammps_logfiles/test_lammps_logfile_parser.py
@@ -0,0 +1,26 @@
+import sys
+import os
+sys.path.insert(0,'../..')
+from simdb.utils import lammps_logfile_parser as llp
+
+
+def total_test():
+    path = 'files'
+    files = os.listdir(path)
+    for file in files:
+        print '\n', file
+
+        L = llp.LogFileReader(os.path.join(path, file))
+        for run in L.runs:
+            print run
+
+def test_production():
+    path = 'files'
+    file = "log.1.lammps"
+    L = llp.LogFileReader(os.path.join(path, file))
+    for run in L.runs:
+        print run
+        print llp.map_lammps_to_database(run)
+
+
+test_production()
