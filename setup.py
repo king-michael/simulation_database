79a2d39af48b5d21e72b97aa96a0f3594cb152e5
Michael King <michael.king@uni-konstanz.de>
2018-07-17 11:24:33 +0200

    init setup.py for #8

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='simulation_database',
+    version='v0.1.0beta',
+    packages=['simdb',
+              'simdb.app',
+              'simdb.utils',
+              ],
+    url='https://github.com/king-michael/simulation_database',
+    license='', # ToDo: Choose a licence
+    author=[
+        'Michael King',
+        'Andrej Berg'
+    ],
+    author_email=[
+        'michael.king@uni-konstanz.de',
+        'andrej.berg@uni-konstanz.de'
+    ],
+    description='', # ToDo: Description
+    install_requires=[
+        'SQLAlchemy',
+        'flask',
+        'Flask-SQLAlchemy',
+    ]
+)
