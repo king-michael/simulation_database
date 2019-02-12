4e8d390c76574adcbe57a1f7eb77019294ba9a3e
Andrej Berg <andrej.berg@uni-konstanz.de>
2019-02-12 21:54:18 +0100

    improve dashboard view

--- simdb/dashboard/app/viewer/routes.py
+++ simdb/dashboard/app/viewer/routes.py
@@ -13,73 +13,47 @@ def viewer_index():
 @blueprint.route('/build_filter')
 def build_filter():
 
-    # interaction with buildFilter() js function
-    db_path = current_app.config['SELECTED_DATABASE']['path']
-    print(db_path)
+    out = {'groups': [],
+           'group_counts': [],
+           'keywords': [],
+           'keyword_counts': [],
+           'values': [],
+           'value_counts': []}
 
-    # get the search query from search field
-    search_query = request.args.get('search_query')
+    # get path to selected database
+    db_path = current_app.config['SELECTED_DATABASE']['path']
 
     # this data is send as coma separated list
-    selected_groups = request.args['selected_group'].split(",")
-    selected_keywords = request.args['selected_keyword'].split(",")
-    selected_keyword_value = request.args['selected_keyword_value'].split(",")
+    selected_groups = request.args.get('selected_groups')
+    selected_keywords = request.args.get('selected_keywords')
+
+    # handle selected groups and keywords
+    if selected_groups == "" or selected_groups == "none":
+        selected_groups = None
+    else:
+        selected_groups = selected_groups.split(",")
+
+    if selected_keywords == "" or selected_keywords == "none":
+        selected_keywords = None
+    else:
+        selected_keywords = selected_keywords.split(",")
+
 
     session = api.connect_database(db_path=db_path)
-    print("------")
-    # get all groups in database
-    groups = api.get_all_groups(session=session)
-    groups = [g[0] for g in groups]
-    print("---", groups)
 
-    # count number of entries for each group
-    groups_count = []
-    query = session.query(Main.entry_id).join(association_main_groups).join(Groups)
-    for g in groups:
-        c = query.filter(Groups.name == g).count()
-        groups_count.append(str(c))
+    # get groups for display
+    groups, counts = zip(*api.get_all_groups(session, count=True))
+    out['groups'], out['group_counts'] = [str(g) for g in groups], list(counts)
 
     # get keywords to display
-    # only show keywords which are present in selected group
-    if selected_groups == [""]:
-        keywords = api.get_all_keywords(session=session)
-    else:
-        query = session.query(Keywords.name, Keywords.value) \
-            .join(Main).join(association_main_groups).join(Groups) \
-            .filter(Groups.name.in_(selected_groups)).distinct()
-        keywords = dict((k, list(zip(*v))[1]) for k, v in itertools.groupby(query.all(), lambda x: x[0]))
+    all_keywords = api.get_all_keywords(session, groups=selected_groups, count=True)
+    out['keywords'], out['keyword_counts'] = zip(*all_keywords)
 
-    # count number of entries for each keyword
-    keywords_count = []
-    query = session.query(Main.entry_id).join(association_main_groups).join(Groups).join(Keywords)
-    if selected_groups == [""]:
-        for k in keywords.keys():
-            c = query.filter(Keywords.name == k).count()
-            keywords_count.append(str(c))
-    else:
-        for k in keywords.keys():
-            c = query.filter(Groups.name.in_(selected_groups), Keywords.name == k).count()
-            keywords_count.append(str(c))
-
-    if len(selected_keywords) > 1:
-        # more than one keyword selected
-        values = []
-    else:
-        if selected_keywords == [""]:
-            # no keyword selected
-            values = []
-        else:
-            # one keyword selected
-            values = keywords[selected_keywords[0]]
+    if selected_keywords:
+        if len(selected_keywords) == 1 and selected_keywords != [""]:
+            out['values'], out['value_counts'] = zip(*api.get_all_keyword_values(session, selected_keywords[0], groups=selected_groups, count=True))
 
     session.close()
-    print(keywords.keys())
-    print(keywords_count)
-    out = {'groups': groups,
-           'groupscount': groups_count,
-           'keywords': keywords.keys(),
-           'keywordscount': keywords_count,
-           'values': values}
 
     return json.dumps(out)
 
@@ -87,114 +61,64 @@ def build_filter():
 def filter_table():
 
     # options which one could change in GUI in the future
-    search_case_sensitive = False
-    used_columns = ["entry_id", "path", "created_on", "added_on", "updated_on", "description"]
+    show_columns = ["entry_id", "path", "created_on", "added_on", "updated_on", "description"]
 
     # load configuration
     db_id = current_app.config['SELECTED_DATABASE']['id']
     db_path = db_path = current_app.config['SELECTED_DATABASE']['path']
 
     # interaction with filterTable() js function
-    # search_query = request.args.get('search_query') or ""
-
-    selected_group = request.args['selected_group']
-    selected_keyword = request.args['selected_keyword']
-    selected_keyword_value = request.args['selected_keyword_value']
-
-    sort_ascending = request.args['sort_ascending']
-    sort_descending = request.args['sort_descending']
-
-    selected_columns = ["entry_id"] + request.args['selected_columns'].split()
+    selected_groups = request.args.get('selected_groups')
+    selected_keywords = request.args.get('selected_keywords')
+    selected_keyword_values = request.args.get('selected_keyword_values')
 
     # some checks on selected path
-    if db_path == "":
-        return "<p style='align=center'>No database selected</p>"
+    if not db_path :
+        return "<h4 style='align=center'><i class='fa fa-info'></i> No database selected. Select a database in Preferences first.</h4>"
     if not os.path.exists(db_path):
-        return "<p style='align=center'>Path to database not found</p>"
+        return "<h4 style='align=center'><i class='fa fa-exclamation'></i> Path to database not found.</h4>"
 
     # handle selected groups and keywords
-    # hotfix until tags get multiselectable
-    if selected_group == "" or selected_group == "none":
-        selected_group = None
+    if selected_groups == "" or selected_groups == "none":
+        selected_groups = None
     else:
-        selected_group = [selected_group]
+        selected_groups = selected_groups.split(",")
 
-    if selected_keyword == "" or selected_keyword == "none":
-        selected_keyword = None
+    if selected_keywords == "" or selected_keywords == "none":
+        selected_keywords = None
     else:
-        selected_keyword = [selected_keyword]
+        selected_keywords = selected_keywords.split(",")
 
-    if selected_keyword_value == "" or selected_keyword_value == "none":
+    if selected_keyword_values == "" or selected_keyword_values == "none":
+        selected_keyword_values = None
+    else:
+        selected_keyword_values = selected_keyword_values.split(",")
+
+    if selected_keyword_values:
+        apply_filter = or_(*[and_(Keywords.name == selected_keywords[0], Keywords.value == value) for value in selected_keyword_values])
+        selected_keywords = None
+    else:
         apply_filter = None
-    else:
-        apply_filter = Main.keywords.any(name=selected_keyword[0], value=selected_keyword_value) # value=selected_keyword_value[0]
-        selected_keyword = None
 
-    # handle sort by
-    if not sort_ascending and not sort_descending:
-        # default values
-        order_by = "id"
-        order = "ascending"
-    elif not sort_ascending:
-        order_by = sort_descending[8:]
-        order = "descending"
-    elif not sort_descending:
-        order_by = sort_ascending[8:]
-        order = "ascending"
-    else:
-        # something went wrong, go to default
-        order_by = "id"
-        order = "ascending"
+    print selected_groups
+    print selected_keywords
+    print selected_keyword_values
 
     # load table
-    # db_id = db.session.query(DBPath).filter(DBPath.path == db_path).one().id  # need this only while working with paths
-    session = api.connect_database(db_path=db_path)
-    table = api.get_entry_table(session, group_names=selected_group, keyword_names=selected_keyword, columns=used_columns, apply_filter=apply_filter, order_by=order_by, order=order)
+    session = api.connect_database(db_path = db_path)
+    table = api.get_entry_table(session,
+                                group_names=selected_groups,
+                                keyword_names=selected_keywords,
+                                columns=show_columns,
+                                apply_filter=apply_filter,
+                                # order_by=order_by,
+                                # order=order
+                                )
     session.close()
 
     # stop if table is empty
     if table.shape[0] == 0:
-        return "<p style='align=center'>No entries found</p>"
-
-    # filter by search query
-    # export this to utils
-    # if search_query != "":
-    #
-    #     # convert search query string to list of statements
-    #     search_for = []
-    #     statement = ''
-    #     split = True  # split words if separated by whitespace
-    #
-    #     for l in search_query.strip():
-    #
-    #         if l == '"':
-    #             # quote starts or ends
-    #             split = not split
-    #
-    #         elif l == ' ' and split:
-    #             # statement ends, append to output
-    #             search_for.append(statement)
-    #             statement = ""
-    #
-    #         else:
-    #             # append letter to statement or whitespace if quote
-    #             statement += l
-    #
-    #     search_for.append(statement)
-    #
-    #     for statement in search_for:
-    #         if search_case_sensitive:
-    #             mask = table['entry_id'].str.contains(statement) | table['description'].str.contains(statement)
-    #         else:
-    #             # (?!) in regex tells re to do a search without case sensitivity
-    #             mask = table['entry_id'].str.contains('(?i)' + statement) | table['description'].str.contains('(?i)' + statement)
-    #         table = table[mask]
-    #         if len(table) == 0:
-    #             return table.to_html(classes=str("table sortable"), escape=False, index=False) # convert to HTML
-    #
-    # # stop if table is empty
-    # if table.shape[0] == 0:
-    #     return "<p style='align=center'>No entries found</p>"
+        return "<h2 style='align=center'><i class='fa fa-info'></i> No entries found</h2>"
 
     # convert table to proper HTML
     pd.set_option('display.max_colwidth', -1) # let pandas print the full entry to HTML table
@@ -210,16 +134,5 @@ def filter_table():
     table["updated_on"] = table["updated_on"].apply(lambda x: x.strftime('%Y/%m/%d'))
     table["added_on"] = table["added_on"].apply(lambda x: x.strftime('%Y/%m/%d'))
     table["created_on"] = table["created_on"].apply(lambda x: x.strftime('%Y/%m/%d') if x is not None else "--")
-
-    # table = table[selected_columns]
-
-    # add buttons for sorting to columns
-    # button_template = '{column_name} <input id="sort_by_{column_name}" type="button" class="sort_button {sort_class}" onClick="sortTable(\'{column_name}\');filterTable()" />'
-    #
-    # html_columns = []
-    # for c in table.columns:
-    #     sort_class = "sort_" + order if c == order_by else ""
-    #     html_columns.append(button_template.format(column_name=c, sort_class=sort_class))
-    # table.columns = html_columns
 
     return table.to_html(table_id="datatable-responsive", classes=str("table"), escape=False, index=False, border=0) # convert to HTML
--- simdb/dashboard/app/viewer/static/build/js/custom.js
+++ simdb/dashboard/app/viewer/static/build/js/custom.js
@@ -23,16 +23,13 @@ function buildFilter(){
         selected_keyword_values.push($(this).val());
     });
 
-    // get search query
-    var search_query = $( "#search_field" ).val();
 
     $.ajax({
         url: "build_filter",                 // call the search page
         data: {
-            search_query: search_query,
-            selected_group: selected_groups.join(","),
-            selected_keyword: selected_keywords.join(","),
-            selected_keyword_value: selected_keyword_values.join(",")
+            // search_query: search_query,
+            selected_groups: selected_groups.join(","),
+            selected_keywords: selected_keywords.join(",")
         },     // get results from search page by sending value
         dataType: "json",
         success: function(data){
@@ -45,33 +42,21 @@ function buildFilter(){
             } else {
                 for(i in data.groups) {
 
-                    var for_display = data.groups[i] + " (" + data.groupscount[i] + ")";
+                    var for_display = data.groups[i] + " (" + data.group_counts[i] + ")";
 
-                    out += "<label class='btn btn-default'>";
-                    out += "<input type='checkbox' name='options' value='" + data.groups[i] + "'>" + for_display;
-                    out += "</label>";
+                    if(  selected_groups.includes(data.groups[i]) ){
+                        out += "<label class='btn btn-default active'>";
+                        out += "<input type='checkbox' name='options' value='" + data.groups[i] + "' checked>" + for_display;
+                        out += "</label>";
+                    } else {
+                        out += "<label class='btn btn-default'>";
+                        out += "<input type='checkbox' name='options' value='" + data.groups[i] + "'>" + for_display;
+                        out += "</label>";
+                    }
                 }
             }
 
             $( "#select_group" ).html(out); // push options to html
-
-            $( "#select_group" ).find( "input:checkbox" ).each(function(){
-                // set checkboxes to active which were selected
-                if(  selected_groups.includes($(this).val()) ){
-                    $(this).prop("checked", true);
-                    $(this).parent().addClass("active");
-                }
-            });
-
-            // add click function:
-            // detect if ctr key was pressed
-            $("#select_group label").click(function(event) {
-                if( ! event.ctrlKey ) {
-                    $("#select_group label").not(this).children().prop("checked", false);
-                    $( "#select_keyword_value label" ).not($(this)).removeClass("active");
-                }
-            });
-
 
             // keywords
             var out = "";
@@ -82,33 +67,21 @@ function buildFilter(){
 
                 for(i in data.keywords) {
 
-                    var for_display = data.keywords[i] + " (" + data.keywordscount[i] + ")";
+                    var for_display = data.keywords[i] + " (" + data.keyword_counts[i] + ")";
 
-                    out += "<label class='btn btn-default'>";
-                    out += "<input type='checkbox' name='options' value='" + data.keywords[i] + "'>" + for_display;
-                    out += "</label>";
+                    if(  selected_keywords.includes(data.keywords[i]) ){
+                        out += "<label class='btn btn-default active'>";
+                        out += "<input type='checkbox' name='options' value='" + data.keywords[i] + "' checked>" + for_display;
+                        out += "</label>";
+                    } else {
+                        out += "<label class='btn btn-default'>";
+                        out += "<input type='checkbox' name='options' value='" + data.keywords[i] + "'>" + for_display;
+                        out += "</label>";
+                    }
                 }
             }
 
             $( "#select_keyword" ).html(out); // push options to html
-
-            $( "#select_keyword" ).find( "input:checkbox" ).each(function(){
-                // set checkboxes to active which were selected
-                if( selected_keywords.includes($(this).val()) ){
-                    $(this).prop("checked", true);
-                    $(this).parent().addClass("active");
-                }
-            });
-
-            // add click function:
-            // detect if ctr key was pressed
-            $("#select_keyword label").click(function(event) {
-                if( ! event.ctrlKey ) {
-                    $("#select_keyword label").not(this).children().prop("checked", false);
-                    $( "#select_keyword_value label" ).not($(this)).removeClass("active");
-                }
-            });
-
 
             // keyword values
             var out = "";
@@ -119,24 +92,21 @@ function buildFilter(){
 
                 for(i in data.values) {
 
-                    var for_display = data.values[i] // + " (" + data.keywordscount[i] + ")";
+                    var for_display = data.values[i]  + " (" + data.value_counts[i] + ")";
 
-                    out += "<label class='btn btn-default'>";
-                    out += "<input type='checkbox' name='options' value='" + data.values[i] + "'>" + for_display;
-                    out += "</label>";
+                    if(  selected_keyword_values.includes(data.values[i]) ){
+                        out += "<label class='btn btn-default active'>";
+                        out += "<input type='checkbox' name='options' value='" + data.values[i] + "' checked>" + for_display;
+                        out += "</label>";
+                    } else {
+                        out += "<label class='btn btn-default'>";
+                        out += "<input type='checkbox' name='options' value='" + data.values[i] + "'>" + for_display;
+                        out += "</label>";
+                    }
                 }
             }
 
             $("#select_keyword_value").html(out); // push options to html
-
-            // add click function:
-            // detect if ctr key was pressed
-            $("#select_keyword_value label").click(function(event) {
-                if( ! event.ctrlKey ) {
-                    $("#select_keyword_value label").not(this).children().prop("checked", false);
-                    $( "#select_keyword_value label" ).not($(this)).removeClass("active");
-                }
-            });
 
         }
 
@@ -149,86 +119,120 @@ function filterTable(){
     // call filter view
     // get table as HTML code back
 
-    // get selected table
-    var db_path = $('#select_database input:radio:checked').val();
-
-    // get selected group
-    var selected_group = $('#select_group input:checkbox:checked').val();
-    if(selected_group == undefined) {
-        selected_group = "";
-    }
-
-    // get selected keyword
-    var selected_keyword = $('#select_keyword input:checkbox:checked').val();
-    if(selected_keyword == undefined) {
-        selected_keyword = "";
-    }
-
-    // get selected keyword value
-    var selected_keyword_value = $( "#select_keyword_value" ).val();
-
-    // get search query
-    var search_query = $( "#search_field" ).val();
-
-
-    // get selected columns with bootstrap select
-    var selected_columns = "";
-    $( "#select_columns" ).find("option:selected").each(function() {
-        selected_columns = selected_columns + " " + $(this).val();
+    // get selected groups
+    var selected_groups = [];
+    $( "#select_group" ).find("input:checkbox:checked").each(function() {
+        selected_groups.push($(this).val());
     });
 
-    // get sort by key
-    var sort_ascending = $( ".sort_ascending" ).attr("id");
-    var sort_descending = $( ".sort_descending" ).attr("id");
-    if(sort_ascending == undefined){
-        sort_ascending = "";
-    }
-    if(sort_descending == undefined){
-        sort_descending = "";
-    }
+    // get selected keywords
+    var selected_keywords = [];
+    $( "#select_keyword" ).find("input:checkbox:checked").each(function() {
+        selected_keywords.push($(this).val());
+    });
 
-    //value = value.trim(); // remove any spaces around the text
-        $.ajax({
-            url: "filter",                 // call the search page
-            data: {                        // get results from search page by sending value
-                db_path: db_path,
-                search_query: search_query,
-                selected_group: selected_group,
-                selected_keyword: selected_keyword,
-                selected_keyword_value: selected_keyword_value,
-                sort_ascending: sort_ascending,
-                sort_descending: sort_descending,
-                selected_columns: selected_columns
-            },
-            dataType: "html",
-            success: function(data){
-                $('#database_entries .x_content').html(data);  // push resulting table to something with id=results
-                $('#database_entries table').dataTable( {
-                    dom: 'fBrtip',
-                    "paging": false,
-                    buttons: [
-                        {
-                            extend: 'csv',
-                            text: "<i class='fas fa-file-csv' aria-hidden='true'></i>"
-                        },
-                        {
-                            extend: 'print',
-                            text: "<i class='fa fa-print' aria-hidden='true'></i>"
-                        },
-                        {
-                            extend: 'colvis',
-                            columns: ':gt(0)',
-                            text: "<i class='fa fa-bars fa-rotate-90' aria-hidden='true'></i>"
-                        }
-                    ]
-                } );
-            }
+    // get selected keyword values
+    var selected_keyword_values = [];
+    $( "#select_keyword_value" ).find("input:checkbox:checked").each(function() {
+        selected_keyword_values.push($(this).val());
+    });
 
-        });
+    $.ajax({
+        url: "filter",                 // call the search page
+        data: {                        // get results from search page by sending value
+            selected_groups: selected_groups.join(","),
+            selected_keywords: selected_keywords.join(","),
+            selected_keyword_values: selected_keyword_values.join(",")
+        },
+        dataType: "html",
+        success: function(data){
+            $('#database_entries .x_content').html(data);  // push resulting table to something with id=results
+            $('#database_entries table').dataTable( {
+                dom: 'fBrtip',
+                "paging": false,
+                buttons: [
+                    {
+                        extend: 'csv',
+                        text: "<i class='fas fa-file-csv' aria-hidden='true'></i>"
+                    },
+                    {
+                        extend: 'print',
+                        text: "<i class='fa fa-print' aria-hidden='true'></i>"
+                    },
+                    {
+                        extend: 'colvis',
+                        columns: ':gt(0)',
+                        text: "<i class='fa fa-bars fa-rotate-90' aria-hidden='true'></i>"
+                    }
+                ]
+            } );
+        }
+
+    });
 }
 
-
+// run on start up
 $(document).ready(function() {
    buildFilter();
    filterTable();
+});
+
+// filter Table after group was selected
+$('#select_group').change(function() {
+    buildFilter();
+    filterTable();
+});
+
+// filter Table after keyword was selected
+$('#select_keyword').change(function() {
+    buildFilter();
+    filterTable();
+});
+
+// filter table after selection of column has changed
+$('#select_keyword_value').change(function () {
+    filterTable();
+});
+
+// buttons for selection
+// $( "#select_keyword" ).find( "input:checkbox" ).each(function(){
+//     // set checkboxes to active which were selected
+//     if( selected_keywords.includes($(this).val()) ){
+//         $(this).prop("checked", true);
+//         $(this).parent().addClass("active");
+//     }
+// });
+
+// $( "#select_group" ).find( "input:checkbox" ).each(function(){
+//     // set checkboxes to active which were selected
+//     if(  selected_groups.includes($(this).val()) ){
+//         $(this).prop("checked", true);
+//         $(this).parent().addClass("active");
+//     }
+// });
+
+
+
+$(document).on('click','#select_group label',function(event) {
+    if( ! event.ctrlKey ) {
+        $( "#select_group label" ).not($(this)).children().prop("checked", false);
+        $( "#select_group label" ).not($(this)).removeClass("active");
+    }
+});
+
+$(document).on('click','#select_keyword label',function(event) {
+    if( ! event.ctrlKey ) {
+        $("#select_keyword label").not($(this)).children().prop("checked", false);
+        $( "#select_keyword label" ).not($(this)).removeClass("active");
+
+        $("#select_keyword_value label").children().prop("checked", false);
+        $( "#select_keyword_value label" ).removeClass("active");
+    }
+});
+
+$(document).on('click','#select_keyword_value label',function(event) {
+    if( ! event.ctrlKey ) {
+        $("#select_keyword_value label").not($(this)).children().prop("checked", false);
+        $( "#select_keyword_value label" ).not($(this)).removeClass("active");
+    }
 });
