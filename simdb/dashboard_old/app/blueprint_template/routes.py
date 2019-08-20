68c463f0db5e98e0e4e7ce2d9296db49107a4525
Michael King <michael.king@uni-konstanz.de>
2019-08-20 18:16:13 +0200

    rm dashboard_old

--- simdb/dashboard_old/app/blueprint_template/routes.py
+++
@@ -1,9 +0,0 @@
-from app.viewer import blueprint
-from flask import render_template, current_app, request
-# import simdb.databaseAPI as api
-
-
-@blueprint.route('/')
-def TEMPLATE_index():
-    return render_template('TEMPLATE_index.html')
-
--- simdb/dashboard_old/app/statistics/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-blueprint = Blueprint(
-    'statistics_blueprint',
-    __name__,
-    url_prefix='/statistics',
-    template_folder='templates',
-    static_folder='static'
-)
